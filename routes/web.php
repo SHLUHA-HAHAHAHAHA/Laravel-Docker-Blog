70a9d0137d288b3f07aafcddfbbb78977fc0e193
Размыслов IS 41 <zalupa@ml.com>
2025-11-05 15:16:25 +0300

    Подгрузка фронт страницы блога

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 'welcome';
+Route::group(['namespace' => "Main"], function ()
+{
+    Route::get('/', 'IndexController');
 });
 
 Auth::routes();
