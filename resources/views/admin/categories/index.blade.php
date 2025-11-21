7d8e10e4fd4ef34db0d5a7aac2eeb7af2eb3d2bc
Ярик домашний пк <razmislovyaroslav@gmail.com>
2025-11-21 18:02:39 +0300

    Урок 8

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -7,7 +7,7 @@
         <div class="container-fluid">
             <!--begin::Row-->
             <div class="row">
-                <div class="col-sm-6"><h3 class="mb-0">Dashboard</h3></div>
+                <div class="col-sm-6"><h3 class="mb-0">Категории</h3></div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-end">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
@@ -26,12 +26,31 @@
         <div class="container-fluid">
             <!--begin::Row-->
             <div class="row">
-                <div class="col-12">
-                    <a href="{{route('admin.category.create')}}" class="btn btn-primary mb-2">Добавить</a>
-                </div>
-                <div class="col-12">
-                    Категории
-                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <div class="col-12">
+                            <a href="{{route('admin.category.create')}}" class="btn btn-primary mb-2">Добавить</a>
+                        </div>
+                        <div class="col-12">
+
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">Название</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($categories as $category)
+                                <tr>
+                                    <th scope="row">{{$category->id}}</th>
+                                    <td>{{ $category->title }}</td>
+                                </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
                 <!--end::Col-->
             </div>
