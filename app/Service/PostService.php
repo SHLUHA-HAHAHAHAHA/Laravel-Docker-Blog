09fdc5342885e879520f3e8e0546b80953eabf92
Размыслов IS 41 <zalupa@ml.com>
2025-11-29 13:11:22 +0300

    Урок 21

---
+++ app/Service/PostService.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Service;
+
+use App\Models\Post;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+
+class PostService
+{
+    public function store($data)
+    {
+        try{
+            DB::beginTransaction();
+            $tagIds = $data['tag_ids'];
+            unset($data['tag_ids']);
+
+            if (isset($data['preview_img'])){
+                $data['preview_img'] = Storage::disk('public')->put('/images', $data['preview_img']);
+            }
+            if (isset($data['main_img'])){
+                $data['main_img'] = Storage::disk('public')->put('/images', $data['main_img']);
+            }
+
+            $post = Post::firstOrCreate($data);
+            $post->tags()->attach($tagIds);
+            DB::commit();
+
+        }catch (\Exception $exception){
+            DB::rollBack();
+            abort(500);
+        }
+    }
+
+    public function update($data, $post)
+    {
+        try {
+            DB::beginTransaction();
+            $tagIds = $data['tag_ids'];
+            unset($data['tag_ids']);
+
+            $data['preview_img'] = Storage::disk('public')->put('/images', $data['preview_img']);
+            $data['main_img'] = Storage::disk('public')->put('/images', $data['main_img']);
+            $post->update($data);
+            $post->tags()->sync($tagIds);
+            DB::commit();
+        }catch (\Exception $exception){
+            DB::rollBack();
+            abort(500);
+        }
+
+        return $post;
+    }
+}
